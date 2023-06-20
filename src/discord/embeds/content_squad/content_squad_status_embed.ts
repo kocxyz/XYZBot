1325c6e52f663cea15bd720370a2677e6bf616b3
Tandashi <18377875+Tandashi@users.noreply.github.com>
2023-06-20 20:10:16 +0200

    fix: Content Squad Status Embed no description error
    An error occured when no description was provided
    which happens in no members were configured.
    This has not been fixed by providing a
    no member found error text.

--- src/discord/embeds/content_squad/content_squad_status_embed.ts
+++ src/discord/embeds/content_squad/content_squad_status_embed.ts
@@ -2,6 +2,12 @@ import { CSMember } from "@prisma/client";
 import { HelixStream } from "@twurple/api";
 import { EmbedBuilder } from "discord.js";
 
+function generateDescription(
+  member: CSMember & { streamData?: HelixStream | null }
+) {
+  return `${member.live ? "🔴" : "⚫"} **[${member.twitchName.toUpperCase()}](https://twitch.tv/${member.twitchName})** - ${member.streamData && member.streamData.gameName ? member.streamData.gameName : 'offline'}`
+}
+
 export function createContentSquadStatusEmbed(
   avatarURL: string,
   members: (CSMember & { streamData?: HelixStream | null })[],
@@ -10,7 +16,9 @@ export function createContentSquadStatusEmbed(
     .setColor(0x808080)
     .setTitle('Creator Squad Members')
     .setDescription(
-      members.map((member) => `${member.live ? "🔴" : "⚫"} **[${member.twitchName.toUpperCase()}](https://twitch.tv/${member.twitchName})** - ${member.streamData && member.streamData.gameName ? member.streamData.gameName : 'offline'}`).join("\n\n")
+      members.length > 0
+        ? members.map((member) => generateDescription(member)).join('\n\n')
+        : 'No Creator Squad Members found.'
     )
     .setThumbnail(avatarURL)
     .setTimestamp()
