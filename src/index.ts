2523d4f6d04e805819a46810e07adf03b7066e04
Tandashi <18377875+Tandashi@users.noreply.github.com>
2023-07-09 01:47:20 +0200

    feat: Add node process error handling

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
 import { DiscordBot } from './discord/bot';
+import { createLogger } from './logging';
+
+const logger = createLogger('Root');
 
 const bot = new DiscordBot();
 bot.connect();
+
+process.on('uncaughtException', function (err) {
+  logger.error(JSON.stringify(err));
+});
+
+process.on('unhandledRejection', function (err) {
+  logger.error(JSON.stringify(err));
+});
